1f23ccc54a645a2186075f81a5a4d2dbe33e1ab0
bardhansuvajit <bardhansuvajit@gmail.com>
2022-03-11 19:56:51 +0530

    mail

--- resources/views/front/product/detail.blade.php
+++ resources/views/front/product/detail.blade.php
@@ -162,11 +162,11 @@
                     <div class="cart-item item-qty">
                         <div class="cart-text">Quantity</div>
                         <div class="qty-box">
-                            <a href="#" class="decrement" type="button">
+                            <a href="javascript: void(0)" class="decrement" type="button">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-minus"><line x1="5" y1="12" x2="19" y2="12"></line></svg>
                             </a>
                             <input class="counter" type="number" value="1" name="qty">
-                            <a href="#" class="increment" type="button">
+                            <a href="javascript: void(0)" class="increment" type="button">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
                             </a>
                         </div>
@@ -187,7 +187,7 @@
             <h6>Details & Specifications</h6>
             <div class="specification">
                 {!! substr($data->desc, 0, 100) !!}
-                <h6><a href="#productDescModal" data-toggle="modal">Read more</a></h6>
+                <h6><a href="javascript: void(0)" data-bs-target="#productDescModal" data-bs-toggle="modal">Read more</a></h6>
             </div>
         </div>
     </div>
