8beeb2eed88e40b271019301784a1de4d5ab4b09
bardhansuvajit <bardhansuvajit@gmail.com>
2022-05-26 16:50:05 +0530

    fixes from server

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -16,11 +16,11 @@ class FrontController extends Controller
     public function index(Request $request)
     {
         // $category = Category::latest('id')->get();
-        $collections = Collection::latest('id')->get();
+        // $collections = Collection::latest('id')->get();
         $products = Product::where('is_trending', 1)->latest('view_count', 'id')->get();
         // $products = Product::latest('view_count', 'id')->limit(16)->get();
         $galleries = Gallery::latest('id')->get();
-        return view('front.welcome', compact('collections', 'products', 'galleries'));
+        return view('front.welcome', compact('products', 'galleries'));
     }
 
     public function mailSubscribe(Request $request)
